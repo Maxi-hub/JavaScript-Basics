6e25080f4973a161a2c3da9f9a9afb2d4fc67c1b
Emma <em.lebedeva85@gmail.com>
2024-08-28 19:45:32 +0300

    Updated after checked

--- JSAPI/HomeWork2/script.js
+++ JSAPI/HomeWork2/script.js
@@ -22,48 +22,73 @@ const prevBtn = document.querySelector('.button-previous');
 const nextBtn = document.querySelector('.button-next');
 
 let index = 0;
-items.innerHTML = `<img src=${images[index]}>`
+// items.innerHTML = `<img src=${images[index]}>`
 
-inputs.forEach((input, index) => {
-    input.addEventListener('change', function (e) {
-        images.forEach((img, imgIndex) => {
-            if (imgIndex === index) {
-                items.innerHTML = `<img src=${images[index]}>`;
-            }
-        });
+// inputs.forEach((input, index) => {
+//     input.addEventListener('change', function (e) {
+//         images.forEach((img, imgIndex) => {
+//             if (imgIndex === index) {
+//                 items.innerHTML = `<img src=${images[index]}>`;
+//             }
+//         });
+//     });
+// });
+
+
+// nextBtn.onclick = () => {
+//     if (index < images.length - 1) {
+//         index++;
+//     }
+//     else {
+//         index = 0;
+//     }
+//     inputs.forEach((input, inputIndex) => {
+//         if (inputIndex !== index) {
+//             input.checked = false;
+//         } else {
+//             input.checked = true;
+//         }
+//     });
+//     items.innerHTML = `<img src=${images[index]}>`
+// }
+
+// prevBtn.onclick = () => {
+//     if (index > 0) {
+//         index--;
+//     } else {
+//         index = images.length - 1;
+//     }
+//     inputs.forEach((input, inputIndex) => {
+//         if (inputIndex !== index) {
+//             input.checked = false;
+//         } else {
+//             input.checked = true;
+//         }
+//     });
+//     items.innerHTML = `<img src=${images[index]}>`
+// }
+
+// Дублирование кода для переключения изображений и обновления радио-кнопок. Можно так написать:
+function updateSlider() {
+    items.innerHTML = `<img src=${images[index]}>`;
+    inputs.forEach((input, inputIndex) => {
+        input.checked = inputIndex === index;
     });
-});
-
+}
 
 nextBtn.onclick = () => {
-    if (index < images.length - 1) {
-        index++;
-    }
-    else {
-        index = 0;
-    }
-    inputs.forEach((input, inputIndex) => {
-        if (inputIndex !== index) {
-            input.checked = false;
-        } else {
-            input.checked = true;
-        }
-    });
-    items.innerHTML = `<img src=${images[index]}>`
-}
+    index = (index + 1) % images.length;
+    updateSlider();
+};
 
 prevBtn.onclick = () => {
-    if (index > 0) {
-        index--;
-    } else {
-        index = images.length - 1;
-    }
-    inputs.forEach((input, inputIndex) => {
-        if (inputIndex !== index) {
-            input.checked = false;
-        } else {
-            input.checked = true;
-        }
+    index = (index - 1 + images.length) % images.length;
+    updateSlider();
+};
+
+inputs.forEach((input, inputIndex) => {
+    input.addEventListener("change", () => {
+        index = inputIndex;
+        updateSlider();
     });
-    items.innerHTML = `<img src=${images[index]}>`
-}
+});
