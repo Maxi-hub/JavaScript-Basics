b7a27759f6db1cf1d5a3f8fc0e877681b1c6aad0
Emma <em.lebedeva85@gmail.com>
2024-12-20 21:18:31 +0300

    Added readme file

---
+++ DOM/intro_to_DOM/README.md
@@ -0,0 +1,30 @@
+# Манипуляция DOM: Поиск, изменение и удаление элементов с помощью JavaScript / DOM manipulation: Searching, changing, and deleting elements using JavaScript
+
+## Описание проекта / Description of the project
+
+Привет! В этом проекте я выполнила несколько заданий, связанных с манипуляцией элементами DOM с использованием JavaScript. Основная цель — продемонстрировать свои навыки в поиске, изменении и удалении элементов на веб-странице.
+Hi! In this project, I have completed several tasks related to manipulating DOM elements using JavaScript. The main goal is to demonstrate your skills in finding, changing, and deleting elements on a web page.
+
+
+## Выполненные задания 
+
+1. Я использовала метод `getElementById` для поиска элемента с id равным "super_link" и вывела этот элемент в консоль.
+2. Внутри всех элементов на странице с классом "card-link" я изменила текст на "ссылка", что позволило обновить содержимое этих элементов.
+3. Я нашла все элементы на странице с классом "card-link", которые находятся внутри элемента с классом "card-body", и вывела полученную коллекцию в консоль.
+4. Я выполнила поиск первого элемента на странице, у которого есть атрибут `data-number` со значением 50, и вывела его в консоль.
+5. Я вывела содержимое тега `title` в консоль, что позволяет видеть заголовок страницы.
+6. Я получила элемент с классом "card-title" и вывела его родительский узел в консоль, что помогает понять структуру DOM.
+7. Я создала тег `p`, записала в него текст "Привет" и добавила созданный тег в начало элемента с классом "card".
+8. Я удалила тег `h6` на странице, что демонстрирует возможность манипуляции элементами DOM.
+
+
+## Completed tasks 
+
+1. I used the 'getElementById` method to search for an element with an id equal to "super_link" and output this element to the console.
+2. Inside all the elements on the page with the "card-link" class, I changed the text to "link", which allowed updating the contents of these elements.
+3. I found all the elements on the page with the "card-link" class that are inside the "card-body" class element, and displayed the resulting collection in the console.
+4. I searched for the first element on the page that has the `data-number` attribute with a value of 50, and output it to the console.
+5. I have displayed the contents of the `title` tag in the console, which allows you to see the page title.
+6. I got an element with the "card-title" class and displayed its parent node in the console, which helps to understand the structure of the DOM.
+7. I created a `p` tag, wrote the text "Hello" into it, and added the created tag to the beginning of the "card" class element.
+8. I deleted the `h6` tag on the page, which demonstrates the possibility of manipulating DOM elements.
--- DOM/intro_to_DOM/script.js
+++ DOM/intro_to_DOM/script.js
@@ -1,45 +1,45 @@
-// 1. Найти по id, используя getElementById, элемент с id равным "super_link" и вывести этот элемент в консоль.
+// 1. Поиск по id, используя getElementById, элемент с id равным "super_link" и вывод этого элемента в консоль.
 const superLink = document.getElementById('super_link');
 const superLinkCopy = document.querySelector('#super_link');
 console.log(superLink);
 console.log(superLinkCopy);
 
-// 2. Внутри всех элементов на странице, которые имеют класс "card-link", поменяйте текст внутри элемента на "ссылка".
+// 2. Внутри всех элементов на странице, которые имеют класс "card-link", изменен текст внутри элемента на "ссылка".
 const cardLink = document.querySelectorAll('.card-link');
 cardLink.forEach(element => {
     element.textContent = 'ссылка';
 });
 
-// 3. Найти все элементы на странице с классом "card-link", которые лежат в элементе с классом "card-body" и вывести полученную коллекцию в консоль.
+// 3. Поиск всех элементов на странице с классом "card-link", которые лежат в элементе с классом "card-body" и вывод полученной коллекции в консоль.
 const cardLinkInCardBody = document.querySelectorAll('.card-body .card-link');
 console.log(cardLinkInCardBody);
 
-// 4. Найти первый попавшийся элемент на странице у которого есть атрибут data-number со значением 50 и вывести его в консоль.
+// 4. Поиск первого попавшегося элемента на странице, у которого есть атрибут data-number со значением 50 и вывод его в консоль.
 const dataNumber50 = document.querySelector('[data-number="50"]');
 console.log(dataNumber50);
 
-// 5. Выведите содержимое тега title в консоль.
+// 5. Вывод содержимого тега title в консоль.
 const title = document.querySelector('title');
 console.log(title);
 
-// 6. Получите элемент с классом "card-title" и выведите его родительский узел в консоль.
+// 6. Получение элемента с классом "card-title" и вывод его родительского узла в консоль.
 const cardTitle = document.querySelector('.card-title');
 console.log(cardTitle.parentElement);
 
-// 7. Создайте тег p`, запишите внутри него текст "Привет" и добавьте созданный тег в начало элемента, который имеет класс "card".
+// 7. Создание тега `p`, запись внутри него текста "Привет" и добавление созданного тега в начало элемента, который имеет класс "card".
 const pElem = document.createElement('p');
 pElem.textContent = 'Привет';
 const card = document.querySelector('.card');
-card.prepend(pElem); 
+card.prepend(pElem); //изменяет DOM, но не возвращает никакого значения. Поэтому при вызове console.log(card.prepend(pElem)) в консоль будет выведено undefined.
+console.log(card.innerHTML);
 
-// 8. Удалите тег h6 на странице.
+// 8. Удаление тега h6 на странице.
 const h6 = document.querySelector('h6');
 h6.remove();
 
-//или, если нужно удалить все теги h6
-// const h6All = document.querySelectorAll('h6'); // сформировался список
-// console.log(h6All);  // убедилась, что тег h6 единственный на странице
-// h6All.forEach(element => { // в данном случае удаление только через цикл
-//     element.remove();
-// });
+// или, если нужно удалить все теги h6
+const h6All = document.querySelectorAll('h6'); 
+h6All.forEach(element => { 
+    element.remove();
+});
 
